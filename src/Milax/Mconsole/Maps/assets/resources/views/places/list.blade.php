8e248e2399e7e08bb326022a9f53bd551d4189ec
Eugene Min <e.min@milax.com>
2016-04-16 12:01:02 +1100

    New add and back buttons

--- src/Milax/Mconsole/Maps/assets/resources/views/places/list.blade.php
+++ src/Milax/Mconsole/Maps/assets/resources/views/places/list.blade.php
@@ -3,8 +3,9 @@
 @section('content')
 
 @include('mconsole::partials.table', [
+    'back' => '/mconsole/maps',
+    'add' => sprintf('/mconsole/maps/%s/places/create', $map->id),
     'title' => sprintf('%s "%s"', trans('mconsole::place.table.placesfor'), $map->name),
-    'actions' => sprintf('<a href="/mconsole/maps" class="btn btn-circle blue"><i class="fa fa-rotate-left"></i> Вернуться назад</a> <a href="/mconsole/maps/%s/places/create" class="btn btn-circle green-jungle"><i class="fa fa-plus"></i> %s </a>', $map->id, trans('mconsole::tables.create')),
 ])
 
 @endsection
