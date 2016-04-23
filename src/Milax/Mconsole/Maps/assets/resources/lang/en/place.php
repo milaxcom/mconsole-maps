c30f37fe4853818258f8f98356aed5eb193b9c9c
Eugene Min <e.min@milax.com>
2016-04-23 17:20:06 +1100

    Maps localization

---
+++ src/Milax/Mconsole/Maps/assets/resources/lang/en/place.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    'menu' => [
+        'list' => [
+            'name' => 'Places',
+            'description' => 'Display a list of places',
+        ],
+        'create' => [
+            'name' => 'Add place',
+            'description' => 'Create places',
+        ],
+        'update' => [
+            'name' => 'Edit',
+            'description' => 'Editing maps and places',
+        ],
+        'delete' => [
+            'name' => 'Delete place',
+            'description' => 'Delete place',
+        ],
+    ],
+    'table' => [
+        'name' => 'Name',
+        'address' => 'Address',
+        'placesfor' => 'Place for map',
+    ],
+    'form' => [
+        'use' => 'Use this place',
+        'picker' => 'Select place on the map',
+        'information' => 'Place information',
+        'latitude' => 'Longitude',
+        'longitude' => 'Latitude',
+        'name' => 'Name',
+        'address' => 'Address',
+        'country' => 'Country',
+        'city' => 'City',
+        'zip' => 'Postal code',
+        'phone' => 'Phone number',
+        'web' => 'Website',
+        'comments' => 'Comments',
+    ],
+];
